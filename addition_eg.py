180e805274f989b34e16448e849787dc67155dd0
manjushaapsunde <manjuapsunde04@gmail.com>
2022-09-02 17:50:21 +0530

    first commit

---
+++ addition_eg.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+def add(num1,num2):
+    return num1 + num2
+
+res = add(num1,num2)
+print(f"Addition of {num1} and {num2} is {res}")
