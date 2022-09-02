d11c631d9884ea50e0b7b66b796d18de0779c8c5
manjushaapsunde <manjuapsunde04@gmail.com>
2022-09-02 17:48:44 +0530

    first commit

---
+++ substraction_eg.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+def sub(num1,num2):
+    return num1 - num2
+
+res = sub(num1,num2)
+print(f"Substraction of {num1} and {num2} is {res}")
